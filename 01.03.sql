78fa944aab5fb35723ba69e4f9fef0e23eb72f46
minks <minkss0901@gmail.com>
2020-01-03 15:22:12 +0900

    gh

--- 01.03.sql
+++ 01.03.sql
@@ -129,9 +129,68 @@ select rownum, ename, sal,deptno
 , rank() over(partition by deptno order by sal desc) 순위
 ,DENSE_RANK() over(partition by deptno order by sal desc) 순위2
 , row_number() over(partition by deptno order by sal desc) 순위3
+, ntile(3) over (order by sal desc) 등분
+, ntile(2) over (order by sal desc) 등분
 from emp;
 
+select ename, sal, hiredate
+,lag(sal, 1) over (order by hiredate) 전sal
+,lag(sal, 2) over (order by hiredate) 전전sal
+,lead(sal, 1) over (order by hiredate) 후sal
+,lead(sal, 2) over (order by hiredate) 후후sal
+from emp;
+
+SELECT empno, ename, deptno, sal
+, sum(sal) over(order by deptno, empno)  
+, sum(sal) over(order by deptno, empno rows between unbounded preceding and unbounded following ) sal2
+, sum(sal) over(order by deptno, empno rows between unbounded preceding and current row ) sal3
+, sum(sal) over(order by deptno, empno rows between current row and unbounded following) sal4
+FROM emp;
+
+select empno,ename, hiredate, sal, deptno 
+, first_value(ename) over(partition by deptno order by sal desc rows between unbounded preceding and unbounded following) 급높
+, last_value(ename) over(partition by deptno order by sal desc rows between unbounded preceding and unbounded following) 급작
+from emp;
+
+select empno,ename, hiredate, sal, deptno 
+, round(ratio_to_report(sal) over(),2) aa
+, trunc(sal/sum(sal)over(),2) cc
+, ratio_to_report(sal) over(partition by deptno) bb
+, trunc(sal / sum(sal) over (partition by deptno),2)
+from emp;
+
+select round(3.1)  반올림, trunc(3.7) 버림,
+ceil(3.1) 무조건정수로올림, 
+floor(3.1) 무조건버림_정수
+from dual;
 
 
+WITH test AS
+(
+SELECT '201901' yyyymm, 100 amt FROM dual
+UNION ALL SELECT '201902', 200 FROM dual
+UNION ALL SELECT '201903', 300 FROM dual
+UNION ALL SELECT '201904', 400 FROM dual
+UNION ALL SELECT '201905', 500 FROM dual
+UNION ALL SELECT '201906', 600 FROM dual
+UNION ALL SELECT '201908', 800 FROM dual
+UNION ALL SELECT '201909', 900 FROM dual
+UNION ALL SELECT '201910', 100 FROM dual
+UNION ALL SELECT '201911', 200 FROM dual
+UNION ALL SELECT '201912', 300 FROM dual
+)
+SELECT yyyymm
+     , amt
+     , SUM(amt) OVER(ORDER BY TO_DATE(yyyymm,'yyyymm')
+                RANGE BETWEEN INTERVAL '3' MONTH PRECEDING
+                          AND INTERVAL '1' MONTH PRECEDING) "직전 3개월 합계"
+     , SUM(amt) OVER(ORDER BY TO_DATE(yyyymm,'yyyymm')
+                RANGE BETWEEN INTERVAL '1' MONTH FOLLOWING
+                          AND INTERVAL '3' MONTH FOLLOWING) "이후 3개월 합계" 
+  FROM test;
 
-
+select level 차수, enmpno, mgr, SYS_CONNECT_BY_PATH(ename, '/'),
+connect_by_isleaf(ename) 마지막level
+from emp
+start with ename = 'KING'
+connect by prior empno = mgr;
